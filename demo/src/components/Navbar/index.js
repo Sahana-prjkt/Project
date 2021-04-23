f4da3a5da56ef10ce3cb01704c64bb983911a4a8
Sahana <shana.suri06@gmail.com>
2021-04-23 09:59:06 +0200

    navbar assignment

---
+++ demo/src/components/Navbar/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <div>
+            <nav id="navid">
+                <a className="decoration" href="">Home</a>
+                <a className="decoration" href="">Sign in</a>
+                <a className="decoration" href="">Contact Us</a>
+            </nav>
+        </div>
+    );
+}
+export default Navbar;
